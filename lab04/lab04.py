e18b6e3bee8b24cba10894ae36c9831f1e05f99b
owen-keating <127237877+owen-keating@users.noreply.github.com>
2023-03-08 21:18:19 +0000

    Add files via upload

---
+++ lab04/lab04.py
@@ -0,0 +1,12 @@
+import turtle
+import time
+turtle.showturtle()
+
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(80)
+
+time.sleep(5)
