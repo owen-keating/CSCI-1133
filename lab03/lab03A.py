09e423932e62e18bbdfc25044ea20d4c549a97c3
owen-keating <127237877+owen-keating@users.noreply.github.com>
2023-03-08 21:18:04 +0000

    Add files via upload

---
+++ lab03/lab03A.py
@@ -0,0 +1,7 @@
+
+first = input("Enter an adjective: ")
+second = input("Enter a past-tense verb: ")
+third = input("Enter a noun: ")
+print("In the 1800's, Ada Lovelace worked with Charles Babbage on a " + first + " engine. ")
+print("While the machine was never " + second + ", her notes include an algorithm intended for it. ")
+print("For this, many consider her to be the first computer " + third + ".")
---
+++ lab03/lab03B.py
@@ -0,0 +1,5 @@
+
+def fill_in(responses):
+    print("In the 1800's, Ada Lovelace worked with Charles Babbage on a " + responses['adjective'] + " engine. ")
+    print("While the machine was never " + responses['past_verb'] + ", her notes include an algorithm intended for it. ")
+    print("For this, many consider her to be the first computer " + responses['noun'] + ".")
